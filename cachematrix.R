2d4a994ffa78242ddd5800046abfc0554f625829
Dani Cucchiara <daniellec@lacertatx.com>
2022-04-22 09:58:48 -0400

    Assignment 2: cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+}
+get <- function() x
+setsolve <- function(solve) s <<- solve
+getsolve <- function() s
+list(set = set, 
+     get = get,
+     setsolve = setsolve,
+     getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+# Part 2 get inverse of matrix x ----
+# Functions will:
+# Check to see the mean has been calculated, then retrieve it from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting the inversed matrix")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
 }
+
+# Also, check it
+NewMatrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+NewMatrix$get()
+NewMatrix$getInverse()
+cacheSolve(NewMatrix)
+NewMatrix$getInverse()
+cacheSolve(NewMatrix)
+NewMatrix$get()
+NewMatrix$getInverse()
+cacheSolve(NewMatrix)
